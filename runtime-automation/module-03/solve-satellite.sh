8d1740701a64aa06c47d9c321ad9c5f1b4539db3
Matthew Yee <yee.matthew@gmail.com>
2026-09-01 07:27:03 -0700

    Add module-03 scaffolding and update module-02 Lightspeed installation content

---
+++ runtime-automation/module-03/solve-satellite.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Solving module-03" >> /tmp/progress.log
+
+# TODO: Add the commands the participant is expected to run during module-03.
+
+echo "Solved module-03" >> /tmp/progress.log
