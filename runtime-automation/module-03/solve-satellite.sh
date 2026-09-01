3218324263b33a82453664de55e2312c24b7e133
Matthew Yee <yee.matthew@gmail.com>
2026-09-01 13:35:41 -0700

    Remove placeholder solve-satellite.sh scripts from module-02 and module-03

--- runtime-automation/module-03/solve-satellite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Solving module-03" >> /tmp/progress.log
-
-# TODO: Add the commands the participant is expected to run during module-03.
-
-echo "Solved module-03" >> /tmp/progress.log
