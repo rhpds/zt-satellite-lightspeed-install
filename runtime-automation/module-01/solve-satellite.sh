540bd3f24af3723bbd82f93cba73f4bffea56d7a
Matthew Yee <yee.matthew@gmail.com>
2026-09-01 13:34:47 -0700

    Update module-03 with RHEL client screenshots and clarify Lightspeed verification steps, remove solve-satellite.sh

--- runtime-automation/module-01/solve-satellite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Solving module-01" >> /tmp/progress.log
-
-# TODO: Add the commands the participant is expected to run during module-01.
-
-echo "Solved module-01" >> /tmp/progress.log
